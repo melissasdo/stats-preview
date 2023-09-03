9d7e8d3b706bbc68a98533e4966fec089a555146
mmercurydevs <melyssa.oliveira@pm.me>
2023-09-03 00:25:20 -0300

    initial files added

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render(__dirname + "/views/index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
